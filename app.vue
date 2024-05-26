a8904a37bfe34653590a3726932b433e7285224b
Alan Tavares Morais <pollargithub@gmail.com>
2024-05-26 17:20:10 -0300

    login com github

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+const { status, data, signIn, signOut } = useAuth();
+
+const loggedIn = computed(() => status.value == "authenticated")
+
+async function handlerSingIn(){
+  await signIn();
+}
+
+async function handlerSingOut(){
+  await signOut();
+}
+</script>
+
 <template>
   <div>
-    hello
+    <button v-if="loggedIn" @click="handlerSingOut">Sing Out</button>
+    <button v-else @click="handlerSingIn">Sing In</button>
   </div>
 </template>
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,8 +4,8 @@ export default NuxtAuthHandler({
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         GithubProvider.default({
-           clientId: 'VHXJKL',
-           clientSecret: 'askdvkaishbxçaosudlablxjnaçuwçdoançskjxboutavouyawibcoyacayusnxçajsxostavei'
+           clientId:"Ov23li83tMgZfP1MVINm",
+           clientSecret:"4ac8f921ad0d3fbf716e2b44c41b4d264cd7b54e"
         })
     ]
 })
